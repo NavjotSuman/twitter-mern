0e8dff761283ffdbd5b14d39d0b971353296c09c
Navjot Suman <navjotsumandev@gmail.com>
2024-08-13 12:34:13 +0530

    7 - add the authorization functionality with in the frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,28 +1,76 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
 import NotificationPage from "./pages/notification/NotificationPage";
 import ProfilePage from "./pages/profile/ProfilePage";
-
+import { Toaster } from "react-hot-toast";
 // comoponents
 
 import Sidebar from "./components/common/Sidebar";
 import RightPanel from "./components/common/RightPanel";
+import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "./components/common/LoadingSpinner";
 
 function App() {
+  const { data: authUser, isLoading } = useQuery({
+    // we use query key to give a unique name to our query and refer it later
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/auth/me");
+        const data = await res.json();
+        if (data.error) {
+          return null;
+        }
+        if (!res.ok) {
+          throw new Error(data.error || "something went wrong");
+        }
+        console.log("auth User is Here : ", data);
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    retry: false,
+  });
+  if (isLoading) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <LoadingSpinner size="lg" />
+      </div>
+    );
+  }
+
   return (
     <>
       <div className="flex max-w-6xl mx-auto">
-        <Sidebar />
+        {authUser && <Sidebar />}
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/notifications" element={<NotificationPage />} />
-          <Route path="/profile/:username" element={<ProfilePage />} />
+          <Route
+            path="/"
+            element={authUser ? <HomePage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/login"
+            element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/signup"
+            // element={<SignUpPage /> }
+            element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/notifications"
+            element={authUser ? <NotificationPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/profile/:username"
+            element={authUser ? <ProfilePage /> : <Navigate to="/" />}
+          />
         </Routes>
-        <RightPanel />
+        {authUser && <RightPanel />}
+        <Toaster />
       </div>
     </>
   );
